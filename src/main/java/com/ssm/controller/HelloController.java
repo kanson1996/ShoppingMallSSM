6dc39eeb7168582ca5ea58872a1aa6c3d24e1086
kanson1996 <935017625@qq.com>
2017-07-24 07:49:53 +0800

    Initial commit

---
+++ src/main/java/com/ssm/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Kanson on 2017/7/24.
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,14 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Kanson
+  Date: 2017/7/24
+  Time: 1:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<h1>${message}</h1><br>
+This is "hello.jsp" page.Congratulations.
+</body>
+</html>
